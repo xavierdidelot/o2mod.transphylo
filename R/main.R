1fd58e0a57f3a59bf7c02f370330549598a783c9
finlaycampbell <f.campbell15@imperial.ac.uk>
2017-09-07 18:03:42 +0100

    accept custom config, moves, etc.

--- R/main.R
+++ R/main.R
@@ -4,9 +4,8 @@
 #' @param sample_every Thining interval
 #' @return results of the run as an outbreaker2 output object
 #' @export
-o2mod.transphylo <- function(data,
-                             n_iter = 1000,
-                             sample_every = 1) {
+o2mod.transphylo <- function(data, config = NULL, priors = NULL,
+                             likelihoods = NULL, moves = NULL) {
   lik_TransPhylo <- function(data, param) {
     ttree <- list(
       ttree = cbind(param$t_inf, data$dates, param$alpha),
@@ -30,10 +29,10 @@ o2mod.transphylo <- function(data,
   }
 
   api <- outbreaker2::get_cpp_api()
-  new_model <-
+  def_likelihoods <-
     outbreaker2::custom_likelihoods(genetic = lik_TransPhylo)
   new_move_tinf <-
-    function(param, data, list_custom_ll = new_model) {
+    function(param, data, list_custom_ll = def_likelihoods) {
       for (i in 1:data$N) {
         current_ll <- api$cpp_ll_all(data, param, i = NULL, list_custom_ll)
         modif <- sample(c(-100:-1, 1:100), 1)
@@ -50,35 +49,48 @@ o2mod.transphylo <- function(data,
       }
       return(param)
     }
-  new_moves <- outbreaker2::custom_moves(t_inf = new_move_tinf)
+  def_moves <- outbreaker2::custom_moves(t_inf = new_move_tinf)
 
   init_tree <- c(NA, rep(1, length(data$dates) - 1))
   init_t_inf <-
     c(min(data$ptree$ptree[, 1]) - 1, data$dates[2:length(data$dates)] - 1)
 
-  conf <-
-    outbreaker2::create_config(
-      init_tree = init_tree,
-      init_t_inf = init_t_inf,
-      init_kappa = 1,
-      init_pi = 1,
-      find_import = FALSE,
-      n_iter = n_iter,
-      sample_every = sample_every,
-      move_mu = FALSE,
-      move_pi = FALSE,
-      move_eps = FALSE,
-      move_lambda = FALSE,
-      move_alpha = FALSE,
-      move_swap_cases = TRUE,
-      move_t_inf = TRUE,
-      move_kappa = FALSE
-    )
+  def_config <- outbreaker2::create_config(init_tree = init_tree,
+                                           init_t_inf = init_t_inf,
+                                           init_kappa = 1,
+                                           init_pi = 1,
+                                           find_import = FALSE,
+                                           n_iter = 1000,
+                                           sample_every = 1,
+                                           move_mu = FALSE,
+                                           move_pi = FALSE,
+                                           move_eps = FALSE,
+                                           move_lambda = FALSE,
+                                           move_alpha = FALSE,
+                                           move_swap_cases = TRUE,
+                                           move_t_inf = TRUE,
+                                           move_kappa = FALSE)
+  
+  def_priors <- outbreaker2::custom_priors()
 
-  res <- outbreaker2::outbreaker(
-    data = data,
-    config = conf,
-    likelihoods = new_model,
-    moves = new_moves
-  )
+  replace_def <- function(default, custom) {
+    for(i in names(custom)) {
+      default[[i]] <- custom[[i]]
+    }
+    return(default)
+  }
+
+  if(!is.null(c(config, priors, likelihoods, moves))) {
+    warning("Modifying default settings may lead to unexpected behaviour, use with caution")
+  }
+  
+  config <- replace_def(def_config, config)
+  priors <- replace_def(def_priors, priors)
+  likelihoods <- replace_def(def_likelihoods, likelihoods)
+  moves <- replace_def(def_moves, moves)
+  
+  res <- outbreaker2::outbreaker(data = data,
+                                 config = config,
+                                 likelihoods = likelihoods,
+                                 moves = moves)
 }
